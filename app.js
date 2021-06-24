91c21a5e95661f791020d22f941e59d751948002
Grigore <grigore.mereacre95@gmail.com>
2021-06-24 12:03:37 -0500

    getproducts fn to get all the the products as json format from lacal files

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
-const closeCartBtn = document.querySelector(".close-cart");
-const cartDom = documnet.querySelector(".cart");
-const cartOverlay = documnet.querySelector(".cart-overlay");
-const cartItems = documnet.querySelector(".cart-items");
-const cartTotal = documnet.querySelector(".cart-total");
-const cartContent = documnet.querySelector(".cart-content");
+const cartDom = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
 const cartProducts = document.querySelector(".product-sector");
 
 let cart = [];
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 class UI {}
 
@@ -18,4 +27,7 @@ class localStorage {}
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+  //gettin all products
+  products.getProducts().then((data) => console.log(data));
 });
+

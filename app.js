876ef8cd12c1038de1799b694ca7465a46c25374
Grigore <grigore.mereacre95@gmail.com>
2021-06-24 11:48:43 -0500

    added all needed variables and setted and event listener for contentloaded

---
+++ app.js
@@ -0,0 +1,21 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const closeCartBtn = document.querySelector(".close-cart");
+const cartDom = documnet.querySelector(".cart");
+const cartOverlay = documnet.querySelector(".cart-overlay");
+const cartItems = documnet.querySelector(".cart-items");
+const cartTotal = documnet.querySelector(".cart-total");
+const cartContent = documnet.querySelector(".cart-content");
+const cartProducts = document.querySelector(".product-sector");
+
+let cart = [];
+class Products {}
+
+class UI {}
+
+class localStorage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});

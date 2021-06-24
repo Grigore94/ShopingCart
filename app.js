db0672d1b5397743787d68b329093f8d4c683f93
Grigore <grigore.mereacre95@gmail.com>
2021-06-24 13:13:49 -0500

    using template litteral adding data to ui foreach method to render evry product

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const cartProducts = document.querySelector(".product-sector");
+const cartProducts = document.querySelector(".products-center");
 
 let cart = [];
 class Products {
@@ -13,28 +13,47 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      let products = data.items
-      products = products.map(item => {
-          const { title, price} = item.fields;
-          const {id} = item.sys
-          const {image} = item.fields.image.fields.file.url
-          return {title, price, id, image}
-      })
-      return products
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const  image  = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
   }
 }
 
-class UI {}
+class UI {
+  displayProducts(products) {
+    console.log(products);
+    let result = "";
+    products.forEach((product) => {
+      result += `
+        <article class="product">
+<div class="img-container">
+    <img src=${product.image} alt="product" class="product-img">
+<button class="bag-btn" data-id=${product.id}>
+    <i class="fa fa-shopping-cart">add to cart</i>
+</button>
+</div>
+<h3>${product.title}</h3>
+<h4>$${product.price}</h4>
+        </article>
+        `;
+    });
+    cartProducts.innerHTML = result;
+  }
+}
 
 class localStorage {}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
-  //gettin all products
-  products.getProducts().then((data) => console.log(data));
+  //gettin all products method
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
-
